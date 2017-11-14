4f94a73781f6d9f7ed57983bc45db990b09575eb
Rosalind Wills <rosalind.m.wills@gmail.comm>
2017-11-14 15:14:50 -0600

    Dashboard refactoring

--- src/display/views/Dashboard.js
+++ src/display/views/Dashboard.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import NumberFormat from 'react-number-format';
 
-import StatsCard from '../components/core/StatsCard/StatsCard';
+import StatsCard from '../components/dashboard/StatsCard/StatsCard';
 import DashboardSummaryHeader from '../components/dashboard/DashboardSummaryHeader/DashboardSummaryHeader';
 import { fetchBudget } from '../../infrastructure/actions';
+import FormattedCurrency from '../components/common/FormattedCurrency';
 
 
 const propTypes = {
@@ -45,64 +45,28 @@ class Dashboard extends Component {
 							<StatsCard
 								bigIcon={<i className="fa fa-dollar text-info savings-card-icon" />}
 								statsText="Planned Savings"
-								statsValue={
-									<NumberFormat
-										decimalScale={2}
-										value={budget ? budget.monthlySavings : ''}
-										displayType="text"
-										thousandSeparator={true}
-										prefix="$"
-										renderText={value => <span>{value}</span>}
-									/>
-								}
+								statsValue={<FormattedCurrency value={budget.monthlySavings} />}
 							/>
 						</Col>
 						<Col lg={3} sm={6}>
 							<StatsCard
 								bigIcon={<i className="fa fa-refresh text-info bills-card-icon" />}
 								statsText="Scheduled Bills"
-								statsValue={
-									<NumberFormat
-										decimalScale={2}
-										value={budget ? budget.monthlyBills : ''}
-										displayType="text"
-										thousandSeparator={true}
-										prefix="$"
-										renderText={value => <span>{value}</span>}
-									/>
-								}
+								statsValue={<FormattedCurrency value={budget.monthlyBills} />}
 							/>
 						</Col>
 						<Col lg={3} sm={6}>
 							<StatsCard
 								bigIcon={<i className="fa fa-arrow-circle-o-down text-info expenses-card-icon" />}
 								statsText="Expenses So Far"
-								statsValue={
-									<NumberFormat
-										decimalScale={2}
-										value={budget ? budget.monthlyExpenses : ''}
-										displayType="text"
-										thousandSeparator={true}
-										prefix="$"
-										renderText={value => <span>{value}</span>}
-									/>
-								}
+								statsValue={<FormattedCurrency value={budget.monthlyExpenses} />}
 							/>
 						</Col>
 						<Col lg={3} sm={6}>
 							<StatsCard
 								bigIcon={<i className="fa fa-arrow-circle-o-up text-info income-card-icon" />}
 								statsText="Expected Income"
-								statsValue={
-									<NumberFormat
-										decimalScale={2}
-										value={budget ? budget.monthlyIncome : ''}
-										displayType="text"
-										thousandSeparator={true}
-										prefix="$"
-										renderText={value => <span>{value}</span>}
-									/>
-								}
+								statsValue={<FormattedCurrency value={budget.monthlyIncome} />}
 							/>
 						</Col>
 					</Row>
